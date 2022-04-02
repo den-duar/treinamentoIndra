6d21a579285d7e30d8b78043c848dd899e46d7f5
den-duar <2021211510079@iesp.edu.br>
2022-04-02 11:35:48 -0300

    Resolvido erros dos imports

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/TransacaoRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/TransacaoRest.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.indracompany.treinamento.model.dto.ConsultarExtratoDTO;
-import com.indracompany.treinamento.model.dto.TransacaoDTO;
 import com.indracompany.treinamento.model.entity.Transacao;
 import com.indracompany.treinamento.model.service.TransacaoService;
 
@@ -38,4 +36,6 @@ public class TransacaoRest extends GenericCrudRest<Transacao, Long, TransacaoSer
 		}
 		return new ResponseEntity<List<Transacao>>(extrato, HttpStatus.OK);
 	}
+	
+	
 }
