f0b3d5e022b247726a1b8e9edb11bbcc2e818662
den-duar <2021211510079@iesp.edu.br>
2022-04-02 15:39:57 -0300

    rotas dos componentes no app principal

--- agencia-angular/src/app/app-routing.module.ts
+++ agencia-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ContasComponent } from './pages/contas/contas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "clientes", component: ClientesComponent
+  },
+  {
+    path: "contas", component: ContasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- agencia-angular/src/app/app.module.ts
+++ agencia-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ContasComponent } from './pages/contas/contas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientesComponent,
+    ContasComponent
   ],
   imports: [
     BrowserModule,
