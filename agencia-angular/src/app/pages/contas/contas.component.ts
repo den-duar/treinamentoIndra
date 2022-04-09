b54fcbf20d6e170642137c8acd8c7fc6de272163
den-duar <2021211510079@iesp.edu.br>
2022-04-09 16:34:58 -0300

    Refeito listagem de contas

--- agencia-angular/src/app/pages/contas/contas.component.ts
+++ agencia-angular/src/app/pages/contas/contas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IConta } from 'src/app/interfaces/conta';
+import { ContasService } from 'src/app/services/contas.service';
 
 @Component({
   selector: 'app-contas',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contasService: ContasService) { }
+  contas: IConta[] = [];
 
   ngOnInit(): void {
+    this.listarTodas();
+
+  }
+
+  listarTodas() {
+    this.contasService.listarTodasContas().subscribe((result: IConta[]) => {
+      this.contas = result;
+    });
   }
 
 }
--- agencia-angular/src/app/services/contas.service.ts
+++ agencia-angular/src/app/services/contas.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IConta } from '../interfaces/conta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContasService {
+  api = environment.api;
+  endpoint = "contas";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  listarTodasContas(){
+    return this.http.get<IConta[]>(`${this.api}/${this.endpoint}/`)
+  }
 }
